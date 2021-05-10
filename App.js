2eadea094ec8cfdb0c68d0e141ce35132cfbe2f1
Tiger Nie <nhl0819@gmail.com>
2021-05-10 13:29:23 +1200

    boilerplate

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import MainTasksScreen from './screens/MainTasksScreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Myelin</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainTasksScreen"
+          component={MainTasksScreen}
+          options={{ title: 'Main Tasks' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ screens/MainTasksScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MainTasksScreen = () => {
+  return (
+    <View>
+      <Text>MainTasksScreen</Text>
+    </View>
+  )
+}
+
+export default MainTasksScreen
