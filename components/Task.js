8195856fffc5c08f27097bac7618a7f668412ff5
bennober <benjaminnober2021@u.northwestern.edu>
2021-05-09 21:31:20 -0500

    stylin

--- components/Task.js
+++ components/Task.js
@@ -1,15 +1,51 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
 
 
 // TODO (L11): conditional styling? 
 
 const Task = ({task}) => (
-    <View>
-        <Text>{task.title}</Text>
-        <Text>Due: {task.dateDue}</Text>
-        <Text>Status: {task.status}</Text>
-    </View>
+    <TouchableOpacity>
+        <View style={styles.container}>
+            <Text style={styles.title}>{task.title}</Text>
+            <Text style={styles.dateDue}>Due: {task.dateDue}</Text>
+            <Text style={styles.status}>Status: {task.status}</Text>
+        </View>
+    </TouchableOpacity>
 )
 
 export default Task
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'left',
+      justifyContent: 'center',
+      marginBottom: 20,
+      marginTop: 10,
+      
+    },
+    title: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'left',
+        justifyContent: 'center',
+        fontSize: 16,
+      },
+    dateDue: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'left',
+        justifyContent: 'center',
+        fontSize: 16,
+    },
+    status: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'left',
+        justifyContent: 'center',
+        fontSize: 16,
+    },
+})
