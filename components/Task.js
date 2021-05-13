8fdfa26844c961ec159105bfa6032c4db1b9dadd
Tiger Nie <nhl0819@gmail.com>
2021-05-13 14:21:50 +1200

    made tasklist style more responsive

--- components/Task.js
+++ components/Task.js
@@ -13,35 +13,42 @@ const Task = ({ task, view }) => (
   </TouchableOpacity>
 )
 
+const row = {
+  fontSize: 16,
+  marginLeft: 10,
+  marginRight: 10,
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'flex-start',
+    alignitems: 'flex-start',
     justifyContent: 'center',
     marginTop: 20,
     paddingTop: 5,
     paddingBottom: 5,
-    paddingLeft: 10,
-    paddingRight: 75,
+    //paddingLeft: 10,
+    //paddingRight: 75,
+    width: '100%',
     borderWidth: 1,
     borderColor: 'black',
     borderRadius: 5,
   },
   title: {
-    fontSize: 16,
+    ...row,
     fontWeight: 500,
   },
   dateDue: {
-    fontSize: 16,
+    ...row,
     fontStyle: 'italic',
   },
   incompleteStatus: {
-    fontSize: 16,
+    ...row,
     color: 'red',
   },
   completeStatus: {
-    fontSize: 16,
+    ...row,
     color: 'green',
   },
 })
--- components/TaskList.js
+++ components/TaskList.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, StyleSheet } from 'react-native'
 import Task from './Task'
 
 const TaskList = ({ tasks, view }) => {
   return (
-    <ScrollView>
-      {tasks.map((task, index) => (
-        <Task key={index} task={task} view={view} />
+    <ScrollView style={styles.container}>
+      {tasks.map((task, idx) => (
+        <Task key={idx} task={task} view={view} />
       ))}
     </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '85%',
+    maxWidth: 800,
+  },
+})
 
 export default TaskList
